8f377c0d4f5b3172ed3203e202b1c8929ce55498
shio <85730998+dino3616@users.noreply.github.com>
2025-01-19 05:51:05 +0900

    feat: ✨ (db) lockers に座標情報をもつ列を追加

---
+++ apps/api/src/infra/prisma/migrations/20250118204752_migration/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "lockers" ADD COLUMN     "lat" DOUBLE PRECISION,
+ADD COLUMN     "lng" DOUBLE PRECISION,
+ADD COLUMN     "location" TEXT;
---
+++ supabase/migrations/20250118204817_migration.sql
@@ -0,0 +1,7 @@
+alter table "public"."lockers" add column "lat" double precision;
+
+alter table "public"."lockers" add column "lng" double precision;
+
+alter table "public"."lockers" add column "location" text;
+
+
