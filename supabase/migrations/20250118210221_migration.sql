68aba0216a84fcd62e4741854de20e8d68c87633
shio <85730998+dino3616@users.noreply.github.com>
2025-01-19 06:04:01 +0900

    fix: 🐛 (db) lockers の緯度経度情報を非 nullable に修正

---
+++ supabase/migrations/20250118210221_migration.sql
@@ -0,0 +1,7 @@
+alter table "public"."lockers" alter column "lat" set not null;
+
+alter table "public"."lockers" alter column "lng" set not null;
+
+alter table "public"."lockers" alter column "location" set not null;
+
+
