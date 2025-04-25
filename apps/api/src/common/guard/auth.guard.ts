3f8bbd85dbe94cca3b0a78041a26ee904dddd814
ReoHakase <st20152ro@gm.ibaraki-ct.ac.jp>
2025-04-26 03:11:08 +0900

    fix: 🐞 (`api`) 開発モードで`AuthGuard`が機能するように修正した。(#18)
    デバッグができなかったため。

--- apps/api/src/common/guard/auth.guard.ts
+++ apps/api/src/common/guard/auth.guard.ts
@@ -1,19 +1,11 @@
 import { type CanActivate, type ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { EnvService } from '#api/common/service/env/env.service';
 import { SupabaseService } from '#api/infra/supabase/supabase.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(
-    private readonly supabaseService: SupabaseService,
-    private readonly envService: EnvService,
-  ) {}
+  constructor(private readonly supabaseService: SupabaseService) {}
 
   async canActivate(context: ExecutionContext) {
-    if (this.envService.NodeEnv === 'development') {
-      return true;
-    }
-
     let accessToken: string | undefined;
     context.getArgs().forEach((arg) => {
       if (arg && arg.req && arg.req.headers) {
