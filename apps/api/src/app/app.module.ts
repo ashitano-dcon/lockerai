1de583afb4149ecaa0a4390d01c09325fb2772f0
koutyuke <gunji.kousuke08@gmail.com>
2025-05-08 02:59:14 +0900

    🎈 feat(api/app): integrate DrizzleModule into app.module for database support

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { CacheModule } from '#api/common/service/cache/cache.module';
 import { EnvModule } from '#api/common/service/env/env.module';
 import { PubSubModule } from '#api/common/service/pubsub/pubsub.module';
 import { GraphQLConfigModule } from '#api/config/graphql/graphql-config.module';
+import { DrizzleModule } from '#api/infra/drizzle/drizzle.module';
 import { IdentificationNnModule } from '#api/infra/identification-nn/identification-nn.module';
 import { LangchainModule } from '#api/infra/langchain/langchain.module';
-import { PrismaModule } from '#api/infra/prisma/prisma.module';
 import { SupabaseModule } from '#api/infra/supabase/supabase.module';
 import { Modules } from '#api/module';
 
@@ -14,12 +14,12 @@ import { Modules } from '#api/module';
   imports: [
     EnvModule,
     GraphQLConfigModule,
-    PrismaModule,
     LangchainModule,
     SupabaseModule,
     IdentificationNnModule,
     CacheModule,
     PubSubModule,
+    DrizzleModule,
     ...Modules,
   ],
 })
