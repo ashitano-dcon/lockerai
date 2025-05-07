cf8e293ff9dd4e45a2652ce4bede7b73d933dabe
koutyuke <gunji.kousuke08@gmail.com>
2025-05-08 02:52:35 +0900

    ✨ feat(api/module/locker/repository): integrate Drizzle client into LockerRepository for data retrieval

--- apps/api/src/module/locker/repository/impl/locker.repository.ts
+++ apps/api/src/module/locker/repository/impl/locker.repository.ts
@@ -1,15 +1,29 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { PrismaService } from '#api/infra/prisma/prisma.service';
+import { inArray } from 'drizzle-orm';
+import { InjectionToken } from '#api/common/constant/injection-token';
+import { DrizzleClient , lockers } from '#api/infra/drizzle';
 import { Locker } from '#api/module/locker/domain/locker.model';
 import type { LockerRepositoryInterface } from '#api/module/locker/repository/locker.repository';
 
+type LockerDto = typeof lockers.$inferSelect;
+
 @Injectable()
 export class LockerRepository implements LockerRepositoryInterface {
-  constructor(@Inject(PrismaService) private readonly prismaService: PrismaService) {}
+  constructor(
+    @Inject(InjectionToken.DRIZZLE_CLIENT)
+    private readonly drizzleClient: DrizzleClient,
+  ) {}
+
+  private mapToLocker(locker: LockerDto): Locker {
+    return new Locker({
+      ...locker,
+      createdAt: new Date(locker.createdAt),
+    });
+  }
 
   async findMany(lockerIds: Parameters<LockerRepositoryInterface['findMany']>[0]): Promise<Locker[]> {
-    const lockers = await this.prismaService.locker.findMany({ where: { id: { in: lockerIds } } });
+    const foundLockers = await this.drizzleClient.select().from(lockers).where(inArray(lockers.id, lockerIds));
 
-    return lockers.map((locker) => new Locker(locker));
+    return foundLockers.map(this.mapToLocker);
   }
 }
