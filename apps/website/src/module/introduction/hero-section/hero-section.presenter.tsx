6c49d4d386f8f1402cff110511520f6f491bcd6c
ReoHakase <st20152ro@gm.ibaraki-ct.ac.jp>
2025-05-09 03:58:55 +0900

    wip: 🚧 (`website`) ナビゲーション関連の関数を`next-intl`のものに置き換えた。(#24)
    - `next/link`由来の素のものを使っていてページ遷移時に言語が切り替わらなかったりエラーが出ていたため。

--- apps/website/src/module/introduction/hero-section/hero-section.presenter.tsx
+++ apps/website/src/module/introduction/hero-section/hero-section.presenter.tsx
@@ -1,13 +1,55 @@
 'use client';
 
 import { BrandLogo } from '@lockerai/core/component/brand-logo';
-import { LinkButton } from '@lockerai/core/component/link-button';
+import { type VariantProps, cn, tv } from '@lockerai/tailwind';
 import { motion } from 'framer-motion';
 import { useTranslations } from 'next-intl';
 import { type ComponentPropsWithoutRef, type ReactNode, useEffect, useState } from 'react';
-import { useRouter } from '#website/i18n/navigation';
+import { Link, useRouter } from '#website/i18n/navigation';
 import { RelateResultDialog } from './component/relate-result-dialog';
 import { SignInDialog } from './component/sign-in-dialog';
+
+const linkButtonVariant = tv({
+  variants: {
+    blur: {
+      true: "relative before:absolute before:inset-0 before:-z-10 before:h-full before:w-full before:rounded-xl before:blur-lg before:content-['']",
+    },
+    border: {
+      true: 'border-2',
+    },
+    color: {
+      purple: 'border-purple-7 bg-purple-3 text-purple-11 before:bg-purple-5 hover:bg-purple-4',
+      cyan: 'border-cyan-7 bg-cyan-3 text-cyan-11 before:bg-cyan-5 hover:bg-cyan-4',
+      green: 'border-green-7 bg-green-3 text-green-11 before:bg-green-5 hover:bg-green-4',
+      sage: 'border-sage-7 bg-sage-3 text-sage-11 before:bg-sage-5 hover:bg-sage-4',
+    },
+    width: {
+      auto: 'w-auto',
+      full: 'w-full',
+    },
+  },
+  defaultVariants: {
+    width: 'auto',
+  },
+});
+
+type LinkButtonProps = ComponentPropsWithoutRef<typeof Link> & {
+  variant?: VariantProps<typeof linkButtonVariant>;
+};
+
+const LinkButton = ({ variant, children, ...props }: LinkButtonProps): ReactNode => (
+  <Link
+    role="button"
+    tabIndex={0}
+    className={cn(
+      'inline-block self-stretch rounded-xl px-7 py-2.5 text-center text-lg font-bold transition tablet:text-xl',
+      linkButtonVariant({ ...variant }),
+    )}
+    {...props}
+  >
+    {children}
+  </Link>
+);
 
 type HeroSectionProps = Omit<ComponentPropsWithoutRef<'section'>, 'children' | 'className'> & {
   asAuth?: boolean;
